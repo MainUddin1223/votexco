42e25e2188127f3f6c5764aa13305fe82267517b
Md Main Uddin <2021md.main@gmail.com>
2025-07-30 14:43:42 +0600

    fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,4 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-import { Button } from "antd";
+
 
 export default function Home() {
   return (
@@ -14,10 +12,10 @@ export default function Home() {
   }}
 >
   <div style={{ textAlign: "center" }}>
-    <h1 style={{fontWeight:'1.5rem',margin:'15px'}}>Welcome to <span style={{color:'red',fontSize:'50px'}}>VOTEXCO,.</span></h1>
+    <h1 style={{fontWeight:'1.5rem',margin:'15px'}}>Welcome to <span style={{color:'red',fontSize:'50px'}}>VOT<span style={{color:'green'}}>Ex</span> CO.,</span></h1>
     <h2 style={{margin:'5px'}}>We are under construction</h2>
     <p>If you have any query contact us</p>
-    <div style={{margin:'5px',fontWeight:"500",fontSize:"25px",color:'blue'}}>
+    <div style={{margin:'5px',fontWeight:"500",fontSize:"25px",color:'blue',}}>
       <p style={{margin:'5px'}}>info@votexco.com</p>
       <p>harun@votexco.com</p>
     </div>
