c2a6a654c43722ed814393eb6a9ebd7230304d57
Md Main Uddin <2021md.main@gmail.com>
2025-07-30 15:00:13 +0600

    fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
   <div style={{ textAlign: "center" }}>
     <h1 style={{fontWeight:'1.5rem',margin:'15px'}}>Welcome to <span style={{color:'red',fontSize:'50px'}}>VOT<span style={{color:'green'}}>Ex</span> CO.,</span></h1>
     <h2 style={{margin:'5px'}}>We are under construction</h2>
-    <p>If you have any query contact us</p>
+    <p>If you have any query please contact us</p>
     <div style={{margin:'5px',fontWeight:"500",fontSize:"25px",color:'blue',}}>
       <p style={{margin:'5px'}}>info@votexco.com</p>
       <p>harun@votexco.com</p>
